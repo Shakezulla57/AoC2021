3ce42361f6d078cb703da9c4aa829eb3e3ca5921
Jack <pilch57@gmail.com>
2021-12-01 13:57:48 -0600

    Day 1 done

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,35 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.filterIndexed { index, s ->
+            if (index == 0) return@filterIndexed false
+            s.toInt() > input[index - 1].toInt()
+        }.count()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+       val summed = (1 until input.lastIndex).mapNotNull { index ->
+            if (index - 1 < 0 || index + 1 >input.lastIndex) {
+                null
+            } else {
+                listOf(
+                    input[index - 1],
+                    input[index],
+                    input[index + 1]
+                ).sumOf { it.toInt() }
+            }
+        }
+        return summed.filterIndexed { index, s ->
+            if (index == 0) return@filterIndexed false
+            s > summed[index - 1]
+        }.count()
     }
+
+
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
